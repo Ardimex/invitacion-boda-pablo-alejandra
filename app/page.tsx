1a0c67c4e2ef10951e4f8d5cda7e85fea30a04b5
Pablo Contreras <42191283+Ardimex@users.noreply.github.com>
2026-04-03 00:45:03 +0000

    Cambia fondo hero

--- app/page.tsx
+++ app/page.tsx
@@ -107,7 +107,7 @@ function HeroSection({
         className="absolute inset-0 bg-cover bg-center"
         style={{
           backgroundImage:
-            'linear-gradient(rgba(255,255,255,0.74), rgba(255,255,255,0.74)), url("https://plus.unsplash.com/premium_photo-1674581928773-0b239c66c385?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
+            'linear-gradient(rgba(255,255,255,0.74), rgba(255,255,255,0.74)), url("https://images.unsplash.com/photo-1520854221256-17451cc331bf?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
         }}
       />
 
